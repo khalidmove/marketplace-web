9af74c41caee8719fe5dda053f39ccd59ef560ba
Rishabh tiwari <rishabhtiwari73096@gmail.com>
2025-02-06 18:27:05 +0530

    update

--- services/service.js
+++ services/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const ConstantsUrl = "http://localhost:3002/v1/api/";
-// export const ConstantsUrl = "https://marketapi.moveex.co/v1/api/";
+// const ConstantsUrl = "http://localhost:3002/v1/api/";
+export const ConstantsUrl = "https://marketapi.moveex.co/v1/api/";
 
 // "https://sp-app-api.herokuapp.com"
 
